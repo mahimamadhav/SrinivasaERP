2929c75a88d974d6a82cf8921ec77823bf46df4a
samba9014 <sambasivaraokonakala96@gmail.com>
2025-05-25 13:26:07 +0530

    login

---
+++ SrinivasaERP/Controllers/LoginController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SrinivasaERP.Models;
+using System.Reflection;
+
+namespace SrinivasaERP.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel Model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("login", Model);
+            }
+            Console.WriteLine(Model.EmployeeIDOrEmail);
+            if ((Model.EmployeeIDOrEmail == "admin" || Model.EmployeeIDOrEmail == "admin@example.com") && Model.Password == "admin123")
+            {
+                TempData["Success"] = "Login successful!";
+                //return RedirectToAction("Dashboard", "Login");
+                return View("login", Model);
+            }
+            else
+            {
+                TempData["Error"] = "Invalid EmployeeID or Password.";
+                return View("login", Model);
+            }
+        }
+        [HttpGet]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+        [HttpPost]
+            
+        public IActionResult ForgotPassword(ForgotModel Model)
+        {
+            Console.WriteLine(Model.EmployeeIDOrEmail);
+            if (!ModelState.IsValid)
+            {
+                return View("forgotpassword", Model);
+            }
+            if (Model.EmployeeIDOrEmail == "admin"|| Model.EmployeeIDOrEmail == "admin@example.com")
+            {
+                TempData["Success"] = " Password reset link has been sent to your email.";
+                return View("forgotpassword");
+            }
+            else
+            {
+                TempData["Error"] = "Invalid EmployeeID or Password.";
+                return View("forgotpassword");
+            }
+            
+        }
+    }
+}
+
---
+++ SrinivasaERP/Models/ForgotModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SrinivasaERP.Models
+{
+    public class ForgotModel
+    {
+        [Required]
+        [Display(Name = "Employee ID or Email")]
+        public string EmployeeIDOrEmail { get; set; }
+    }
+}
