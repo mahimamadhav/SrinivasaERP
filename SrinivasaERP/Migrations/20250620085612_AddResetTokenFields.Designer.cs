417769d4c59ffd7bcd6f2091f31a9c10e827f5e3
samba9014 <sambasivaraokonakala96@gmail.com>
2025-06-20 15:15:20 +0530

    forgotpassword completed

--- SrinivasaERP/Migrations/20250620085612_AddResetTokenFields.Designer.cs
+++ SrinivasaERP/Migrations/20250620085612_AddResetTokenFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SrinivasaERP.Data;
 
@@ -11,9 +12,11 @@ using SrinivasaERP.Data;
 namespace SrinivasaERP.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250620085612_AddResetTokenFields")]
+    partial class AddResetTokenFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SrinivasaERP/Models/ResetPasswordModel .cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SrinivasaERP.Models
+{
+    public class ResetPasswordModel
+    {
+        public string? Token { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? NewPassword { get; set; }
+
+        [Required]
+        [Compare("NewPassword", ErrorMessage = "Passwords do not match.")]
+        [DataType(DataType.Password)]
+        public string? ConfirmPassword { get; set; }
+    }
+}
